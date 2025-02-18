0c8d946f5be43cf717a7a6b69b867f50f6f0e229
Jitender K <jkumar@ci.com>
2025-02-18 16:07:55 +0000

    added new model changes

---
+++ models/staging/stripe/stg_payment.sql
@@ -0,0 +1,11 @@
+with payment as(
+    select 
+        id as payment_id,
+        orderid,
+        paymentmethod,
+        status,
+        amount,
+        created
+        from {{source('stripe','payment')}}    
+)
+select * from payment
